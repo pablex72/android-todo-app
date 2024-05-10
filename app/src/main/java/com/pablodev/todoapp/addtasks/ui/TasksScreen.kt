1ed919b2683b4dbab4c2c61be2ceedfbd930d9aa
pablo <tonyp2hoy@gmail.com>
2024-05-10 19:21:24 -0400

    feat: adding removing taks

--- app/src/main/java/com/pablodev/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/pablodev/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,16 +1,26 @@
 package com.pablodev.todoapp.addtasks.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -24,10 +34,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.pablodev.todoapp.addtasks.ui.model.TaskModel
 
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
@@ -42,9 +54,49 @@ fun TasksScreen(tasksViewModel: TasksViewModel) {
         FabDialog(
             Modifier
                 .align(Alignment.BottomEnd)
-                .padding(16.dp), tasksViewModel)
-
+                .padding(16.dp), tasksViewModel
+        )
+        TasksList(tasksViewModel)
     }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+
+    val myTasks: List<TaskModel> = tasksViewModel.task
+    LazyColumn {
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(task, tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit) {
+               detectTapGestures (onLongPress = {
+                   tasksViewModel.onItemRemove(taskModel)
+               })
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task, modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .weight(1f)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { tasksViewModel.onCheckBoxSelected(taskModel) })
+        }
+    }
+
 }
 
 @Composable
@@ -85,6 +137,7 @@ fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -
 
                 Button(onClick = {
                     onTaskAdded(myTask)
+                    myTask = ""
                 }, modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Aniadir tarea")
                 }
---
+++ app/src/main/java/com/pablodev/todoapp/addtasks/ui/model/TaskModel.kt
@@ -0,0 +1,5 @@
+package com.pablodev.todoapp.addtasks.ui.model
+
+data class TaskModel(val id:Long = System.currentTimeMillis(), val task:String, var selected:Boolean=false) {
+
+}
